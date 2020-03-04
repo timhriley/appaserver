2a14fb86d5253b3804a6f51c3aaa7d23eb2bc362
Tim Riley <timriley@appahost.com>
2020-03-04 09:03:43 -0800

    added debugging messages

--- src_hydrology/station_datatype.c
+++ src_hydrology/station_datatype.c
@@ -875,7 +875,6 @@ boolean station_datatype_expected_count_reject(
 
 		if ( minutes > 45 || remainder )
 		{
-/*
 fprintf( stderr, "%s/%s()/%d: input: measurement_time = %s, expected_count = %d; output: rejecting %d minutes with remainder = %d\n",
 __FILE__,
 __FUNCTION__,
@@ -885,21 +884,20 @@ expected_count_per_day,
 minutes,
 remainder );
 fflush( stderr );
-*/
 
 			return 1;
 		}
 		else
 		{
-/*
-fprintf( stderr, "%s/%s()/%d: accepting %d minutes with remainder = %d\n",
+fprintf( stderr, "%s/%s()/%d: input: measurement_time = %s, expected_count = %d; output: accepting %d minutes with remainder = %d\n",
 __FILE__,
 __FUNCTION__,
 __LINE__,
+measurement_time,
+expected_count_per_day,
 minutes,
 remainder );
 fflush( stderr );
-*/
 
 			return 0;
 		}
