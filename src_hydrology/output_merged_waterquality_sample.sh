efe6ee7b5ca912cab323593023bc3cf843dda3c4
Tim Riley <timriley@appahost.com>
2020-03-17 12:41:38 -0700

    expanded output

--- src_hydrology/output_merged_waterquality_sample.sh
+++ src_hydrology/output_merged_waterquality_sample.sh
@@ -1,11 +1,12 @@
 :
-output_merged_waterquality						 \
-	output_merged_waterquality					 \
-	2008-01-01							 \
-	2008-12-31							 \
-	stdout								 \
-	"station^datatype_1=BK^stage&					 \
-	station^datatype_2=WB^stage&					 \
-	waterquality_station_parameter_1=FLAB10^CHLOROPHYLL-A^mg/m**3"
+output_merged_waterquality						\
+	output_merged_waterquality					\
+	2008-01-01							\
+	2008-12-31							\
+	stdout								\
+	"station^datatype_1=BK^stage&					\
+	station^datatype_2=WB^stage&					\
+	waterquality_station_parameter_1=FLAB10^CHLOROPHYLL-A^mg/m**3&	\
+	waterquality_station_parameter_2=FLAB11^CHLOROPHYLL-A^mg/m**3"
 
 exit 0
