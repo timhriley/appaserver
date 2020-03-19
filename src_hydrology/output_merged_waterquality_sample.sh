72d15f0fcfdff350f334f02452db253ce2f6e4b2
Tim Riley <timriley@appahost.com>
2020-03-19 10:41:52 -0400

    better test data

--- src_hydrology/output_merged_waterquality_sample.sh
+++ src_hydrology/output_merged_waterquality_sample.sh
@@ -1,11 +1,15 @@
 :
-output_merged_waterquality						 \
-	output_merged_waterquality					 \
-	2008-01-01							 \
-	2008-12-31							 \
-	stdout								 \
-	"station^datatype_1=BK^stage&					 \
-	station^datatype_2=WB^stage&					 \
-	waterquality_station_parameter_1=FLAB10^CHLOROPHYLL-A^mg/m**3"
+output_merged_waterquality						\
+	output_merged_waterquality					\
+	2008-01-01							\
+	2008-07-01							\
+	stdout								\
+	"station^datatype_1=BK^salinity&				\
+	 station^datatype_2=GB^salinity&				\
+	 station^datatype_3=JK^salinity&				\
+	 station^datatype_4=LR^salinity&				\
+	 station^datatype_5=MK^salinity&				\
+	 waterquality_station_parameter_1=FLAB10^CHLOROPHYLL-A^mg/m**3&	\
+	 waterquality_station_parameter_2=FLAB11^CHLOROPHYLL-A^mg/m**3"
 
 exit 0
