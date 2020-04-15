74a8f633955701960d484c2125165cf7f4689fdd
Tim Riley <timriley@appahost.com>
2020-04-15 09:03:28 -0700

    restore a huge line backup by splitting it up first

--- utility/bigrestore.sh
+++ utility/bigrestore.sh
@@ -1,49 +1,66 @@
 :
-if [ "$#" -ne 2 ]
+if [ "$#" -lt 2 ]
 then
-	echo "Usage: $0 table yyyymmdd" 1>&2
+	echo "Usage: $0 table yyyymmdd [resume_sql]" 1>&2
 	exit 1
 fi
 
 table=$1
 date=$2
 
+if [ "$#" -eq 3 ]
+then
+	resume_sql=$3
+fi
+
 create_sql="create.sql"
 insert_sql="insert.sql"
 
-bigrestore_grep_create.sh $table $date > $create_sql
-
-if [ $? -ne 0 ]
+if [ "$resume_sql" = "" ]
 then
-	echo "$0: bigrestore_grep_create.sh exiting early" 1>&2
-	exit 1
+	bigrestore_grep_create.sh $table $date > $create_sql
+
+	if [ $? -ne 0 ]
+	then
+		echo "$0: bigrestore_grep_create.sh exiting early" 1>&2
+		exit 1
+	fi
 fi
 
-bigrestore_grep_insert.sh $table $date > $insert_sql
-
-if [ $? -ne 0 ]
+if [ "$resume_sql" = "" ]
 then
-	echo "$0: bigrestore_grep_insert.sh exiting early" 1>&2
-	exit 1
+	bigrestore_grep_insert.sh $table $date > $insert_sql
+
+	if [ $? -ne 0 ]
+	then
+		echo "$0: bigrestore_grep_insert.sh exiting early" 1>&2
+		exit 1
+	fi
 fi
 
-bigrestore_split.sh $insert_sql
-
-if [ $? -ne 0 ]
+if [ "$resume_sql" = "" ]
 then
-	echo "$0: bigrestore_split.sh exiting early" 1>&2
-	exit 1
+	bigrestore_split.sh $insert_sql
+
+	if [ $? -ne 0 ]
+	then
+		echo "$0: bigrestore_split.sh exiting early" 1>&2
+		exit 1
+	fi
 fi
 
-cat $create_sql | sql.e
-
-if [ $? -ne 0 ]
+if [ "$resume_sql" = "" ]
 then
-	echo "$0: sql.e exiting early" 1>&2
-	exit 1
+	cat $create_sql | sql.e
+
+	if [ $? -ne 0 ]
+	then
+		echo "$0: sql.e exiting early" 1>&2
+		exit 1
+	fi
 fi
 
-bigrestore_insert.sh
+bigrestore_insert.sh "$resume_sql"
 
 if [ $? -ne 0 ]
 then
--- utility/bigrestore_insert.sh
+++ utility/bigrestore_insert.sh
@@ -1,9 +1,24 @@
 :
 
+if [ $# -eq 1 ]
+then
+	resume_sql=$1
+fi
+
 for file in xa*.sql
 do
+	if [ "$resume_sql" != "" ]
+	then
+		results=`strcmp.e $file $resume_sql`
+
+		if [ $results -lt 0 ]
+		then
+			continue
+		fi
+	fi
+
 	echo "$file"
-	cat $file | sql.e
+	cat $file | sql.e 2>&1 | grep -vi duplicate
 done
 
 exit 0
