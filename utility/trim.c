aac6cec905787892325d2c3d576e5bbeef8c5fc4
Tim Riley <timriley@appahost.com>
2020-05-05 13:15:09 -0400

    trim can now truncate strings

--- utility/trim.c
+++ utility/trim.c
@@ -7,14 +7,29 @@
 #include <stdlib.h>
 #include "timlib.h"
 
-int main( void )
+int main( int argc, char **argv )
 {
 	char buffer[ 65536 ];
+	char *ptr;
+	int how_many = 0;
+
+	if ( argc == 2 )
+	{
+		how_many = atoi( argv[ 1 ] );
+	}
 
 	while( get_line( buffer, stdin ) )
 	{
-		printf( "%s\n", trim( buffer ) );
+		ptr = trim( buffer );
+
+		if ( how_many )
+		{
+			*( ptr + how_many ) = '\0';
+		}
+
+		printf( "%s\n", buffer );
 	}
+
 	return 0;
 
 } /* main() */
