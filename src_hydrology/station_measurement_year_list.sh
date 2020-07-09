569078db979b1aad1b8c94092f0f4147f6f064e3
Tim Riley <timriley@appahost.com>
2020-07-09 09:53:24 -0700

    have current vs historical's map filter stations for the year

---
+++ src_hydrology/station_measurement_year_list.sh
@@ -0,0 +1,36 @@
+:
+
+if [ "$#" -ne 1 ]
+then
+	echo "Usage: $0 year" 1>&2
+	exit 1
+fi
+
+year=$1
+
+s=station
+sd=station_datatype
+m=measurement
+
+select="$sd.station, $s.station_type, $sd.datatype"
+
+from="$sd, $s"
+
+join="$sd.station = $s.station"
+
+filter="$s.station_type is not null"
+
+where="$join and $filter and 						\
+	exists (select 1						\
+		from $m							\
+		where $m.station = $sd.station				\
+		  and $m.datatype = $sd.datatype			\
+		  and $m.measurement_date between '$year-01-01'		\
+		  and				   '$year-12-31')"
+
+echo "select $select from $from where $where order by $select;"		|
+tee /dev/tty |
+sql.e
+
+exit 0
+
