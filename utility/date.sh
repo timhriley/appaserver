fd5ff07f328efae5c552e242a3f3465650e848e8
Tim Riley <timriley@appahost.com>
2020-07-22 16:29:30 +0000

    override local time zone; use UTC_OFFSET instead

--- utility/date.sh
+++ utility/date.sh
@@ -4,7 +4,8 @@
 
 if [ "$#" -eq 0 ]
 then
-	TZ=`appaserver_tz.sh` /bin/date '+%m/%d/%Y %H:%M'
+	#TZ=`appaserver_tz.sh` /bin/date '+%m/%d/%Y %H:%M'
+	TZ=`appaserver_tz.sh` date.e 0 | sed 's/:/ /' | sed 's/[0-9][0-9]$/:&/'
 else
 	days_offset=$1
 
