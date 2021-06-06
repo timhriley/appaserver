3105063db4d4d4d73cbfb0d560927fa01b65e8f1
Tim Riley <timriley@appahost.com>
2021-06-06 13:32:51 -0700

    rewriting tax recovery module

--- src_predictive/recovery_statutory_accelerated_half_year.sh
+++ src_predictive/recovery_statutory_accelerated_half_year.sh
@@ -4,21 +4,6 @@
 # Freely available software. See appaserver.org
 # ----------------------------------------------------------
 
-if [ "$APPASERVER_DATABASE" != "" ]
-then
-	application=$APPASERVER_DATABASE
-elif [ "$DATABASE" != "" ]
-then
-	application=$DATABASE
-fi
-
-if [ "$application" = "" ]
-then
-	echo "Error in `basename.e $0 n`: you must first:" 1>&2
-	echo "\$ . set_database" 1>&2
-	exit 1
-fi
-
 if [ $# -lt 1 ]
 then
 	echo "Usage: $0 years [tax_year]" 1>&2
