653fd271207f70bf59c939bb835b982c7fd8332f
Tim Riley <timriley@appahost.com>
2022-06-12 14:47:35 -0700

    rewriting prompt process

--- src_appaserver/post_choose_process.c
+++ src_appaserver/post_choose_process.c
@@ -10,59 +10,29 @@
 #include "timlib.h"
 #include "list.h"
 #include "session.h"
-#include "appaserver_library.h"
-#include "appaserver_error.h"
-#include "appaserver_user.h"
-#include "environ.h"
-#include "dictionary.h"
-#include "post2dictionary.h"
-#include "appaserver_parameter_file.h"
+#include "post_choose_process.h"
 
 int main( int argc, char **argv )
 {
-	char *login_name, *application_name, *session, *process;
+	char *application_name;
+	char *session_key;
+	char *login_name;
 	char *role_name;
-	char sys_string[ 1024 ];
-	LIST *process_parameter_record_list;
-	LIST *preprompt_process_parameter_record_list;
-	int is_process_set;
-	char *database_string = {0};
+	char *process_or_set_name;
 
 	if ( argc != 6 )
 	{
-		fprintf( stderr, 
-"Usage: %s login_name application session process role\n",
-		argv[ 0 ] );
+		fprintf(stderr, 
+		"Usage: %s application session login role process_or_set\n",
+			argv[ 0 ] );
 		exit ( 1 );
 	}
 
-	login_name = argv[ 1 ];
-	application_name = argv[ 2 ];
-	session = argv[ 3 ];
-	process = argv[ 4 ];
-	role_name = argv[ 5 ];
-
-	if ( timlib_parse_database_string(	&database_string,
-						application_name ) )
-	{
-		environ_set_environment(
-			APPASERVER_DATABASE_ENVIRONMENT_VARIABLE,
-			database_string );
-	}
-	else
-	{
-		environ_set_environment(
-			APPASERVER_DATABASE_ENVIRONMENT_VARIABLE,
-			application_name );
-	}
-
-	add_src_appaserver_to_path();
-	environ_set_utc_offset( application_name );
-
-	appaserver_output_starting_argv_append_file(
-		argc,
-		argv,
-		application_name );
+	application_name = argv[ 1 ];
+	session_key = argv[ 2 ];
+	login_name = argv[ 3 ];
+	role_name = argv[ 4 ];
+	process_or_set = argv[ 5 ];
 
 	environ_prepend_dot_to_path();
 	add_utility_to_path();
