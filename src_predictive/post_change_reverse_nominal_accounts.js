0cc40acbb639335a8341099cd2ffc48a171ee76c
Tim Riley <timriley@appahost.com>
2025-09-11 09:56:08 -0700

    implementing reverse nominal accounts

---
+++ src_predictive/post_change_reverse_nominal_accounts.js
@@ -0,0 +1,24 @@
+// $APPASERVER_HOME/src_predictive/post_change_reverse_nominal_accounts.js
+// -----------------------------------------------------------------------
+// No warranty and freely available software. Visit appaserver.org
+// -----------------------------------------------------------------------
+function post_change_reverse_nominal_accounts()
+{
+	var element;
+	var value;
+
+	element = timlib_element( "reverse_date" );
+	element.disabled = 0;
+
+	element = timlib_element( "undo_yn" );
+	value = timlib_drop_down_element_value( element.options );
+
+	if ( value == 'y' )
+	{
+		element = timlib_element( "reverse_date" );
+		element.disabled = 1;
+	}
+
+	return true;
+}
+
