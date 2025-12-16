3e4746bc3b32177afa5623074fb5d01d2199d2ff
Tim Riley <timriley@appahost.com>
2025-12-16 13:17:52 -0800

    pay liabilities: mutually exclusive account or starting_check_number

--- src_predictive/post_change_pay_liabilities.js
+++ src_predictive/post_change_pay_liabilities.js
@@ -7,22 +7,43 @@ function post_change_pay_liabilities( current_element )
 	var element;
 	var value;
 
-	element = timlib_get_element( "memo" );
+	element = timlib_element( "account" );
+	element.disabled = 0;
+
+	element = timlib_element( "starting_check_number" );
+	element.disabled = 0;
+
+	element = timlib_element( "memo" );
 	element.disabled = 1;
 
-	element = timlib_get_element( "with_stub_yn" );
-	element.disabled = 1;
-
-	element = timlib_get_element( "starting_check_number" );
+	// If starting_check_number is set
+	// then no account and yes memo.
+	// --------------------------------
+	element = timlib_element( "starting_check_number" );
 	value = element.value;
 
 	if ( value >= 1 )
 	{
-		element = timlib_get_element( "memo" );
+		element = timlib_element( "memo" );
 		element.disabled = 0;
 
-		element = timlib_get_element( "with_stub_yn" );
-		element.disabled = 0;
+		element = timlib_element( "account" );
+		element.disabled = 1;
+
+		return true;
+	}
+
+	// If account is set
+	// then no starting_check_number.
+	// ------------------------------
+	element = timlib_element( "account" );
+
+	if ( timlib_drop_down_element_populated( element.options ) )
+	{
+		element = timlib_element( "starting_check_number" );
+		element.disabled = 1;
+
+		return true;
 	}
 
 	return true;
