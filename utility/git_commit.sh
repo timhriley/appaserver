ee0e845c6eb748588c24f89d65ddd39b6df28929
Tim Riley <timriley@appahost.com>
2026-06-10 23:03:22 -0700

    implementing entity_key

--- utility/git_commit.sh
+++ utility/git_commit.sh
@@ -1,17 +1,28 @@
 :
 # $APPASERVER_HOME/utility/git_commit.sh
 # --------------------------------------
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-	echo "Usage: $0 \"message\"" 1>&2
+	echo "Usage: $0 \"message\" [recomile_yn]" 1>&2
 	exit 1
 fi
 
 message="$1"
 
+if [ "$#" -eq 2 ]
+then
+	recompile_yn="$2"
+else
+	recompile_yn="y"
+fi
+
 cd $APPASERVER_HOME
 make clean >/dev/null
 git commit -am "$message"
-make >/dev/null
+
+if [ "$recompile_yn" = 'y' ]
+then
+	make >/dev/null
+fi
 
 exit $?
