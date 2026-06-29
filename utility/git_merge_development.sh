eb735abc5b2d5b810a1706e4a4825dc68fc1519f
Tim Riley <timriley@appahost.com>
2026-06-28 18:02:21 -0700

    touched git shell scripts

--- utility/git_merge_development.sh
+++
@@ -1,8 +0,0 @@
-:
-# $APPASERVER_HOME/utility/git_merge_development.sh
-# -------------------------------------------------
-git checkout development
-git merge main
-git checkout main
-
-exit 0
--- utility/git_merge_main.sh
+++ utility/git_merge_main.sh
@@ -1,8 +1,19 @@
 :
 # $APPASERVER_HOME/utility/git_merge_main.sh
 # ------------------------------------------
+
+current_branch=$(git branch | grep '^*' | column.e 1)
+
+if [ "$current_branch" = "main" ]
+then
+	echo "ERROR in `basename.e $0 n`: You are on branch main." 1>&2
+	exit 1
+fi
+
 git checkout main
-git merge development
-git checkout development
+git merge $current_branch
+git checkout $current_branch
+
+echo "Branch main is now caught up with this branch."
 
 exit 0
