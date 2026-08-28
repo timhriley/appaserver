2e99e8a8d4eeb72ddba88f32cc75c4920c19174c
Tim Riley <timriley@appahost.com>
2026-08-28 14:30:57 -0700

    regenerate import statement

--- src_predictive/specific_inventory_purchase.h
+++ src_predictive/specific_inventory_purchase.h
@@ -118,6 +118,19 @@ double specific_inventory_purchase_list_total(
 
 /* Usage */
 /* ----- */
+void specific_inventory_purchase_list_set_update_string(
+		const char sql_delimiter,
+		char *fund_name,
+		char *full_name,
+		char *contact_key,
+		char *purchase_date_time,
+		boolean predictive_fund_boolean,
+		boolean entity_contact_key_boolean,
+		LIST *specific_inventory_purchase_list
+			/* Set each update_string_list */ );
+
+/* Usage */
+/* ----- */
 LIST *specific_inventory_purchase_list_update_string_list(
 		LIST *specific_inventory_purchase_list );
 
