ceed2277eb87cecddedbf7be115f818f0ea0c07c
Tim Riley <timriley@appahost.com>
2026-09-12 11:33:46 -0700

    home edition includes table=SELECT_STATEMENT

---
+++ utility/memory_process.sh
@@ -0,0 +1,9 @@
+:
+# $APPASERVER_HOME/utility/memory_process.sh
+# ---------------------------------------------------------------
+# No warranty and freely available software. Visit appaserver.org
+# ---------------------------------------------------------------
+
+ps -auxww --sort=-%mem
+
+exit 0
